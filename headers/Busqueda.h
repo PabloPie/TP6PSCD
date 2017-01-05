e5a748fb7d1619ec60de32e5f827c994a67d1518
Beatriz <Beatriz@Bea.Home>
2017-01-05 22:56:19 +0100

    commit inicial

---
+++ headers/Busqueda.h
@@ -0,0 +1,20 @@
+
+#include "Monumento.h"
+
+/*
+ * Pre: c != null y n<= que la longitud de c.
+ * Post: Devuelve la posicion en la que se encuentre el valor minimo de c en el 
+ * rango [0,n], en caso de haber minimos identicos devuelve la posicion del 
+ * primero. 
+ */
+int posDeReemplazo (int* c, int n);
+
+/*
+ * Pre: m!= null y n<= que la longitud de m, resultado!= null y n2 = la longitud
+ * de resultado, a es un monumento con no más de 5 terminos definidos.
+ * Post: Busca en la tabla m monumetos que tengan al menos 1 coincidencia y 
+ * rellena la tabla resultado de forma que nos devuelva los Monumentos con mayor
+ * indice de proximidad al Monumento a, ordenados de mayor a menor.
+ * Devuelve el número de elementos encontrados. 
+ */
+int busquedaMonumento(Monumento* m,Monumento* resultado, Monumento& a, int n, int n2);
---
+++ src/Busqueda.cpp
@@ -0,0 +1,67 @@
+/*
+ * Autores:
+ * Nip:
+ * Fecha de entrega: 10/01/2017
+ */
+#include "../headers/Busqueda.h"
+
+using namespace std;
+        
+int posDeReemplazo (int* c, int n){
+    int pos = 0;
+    int min = c[pos];
+    int i = 1;
+    while(min != 0 && i<n){
+        if(min > c[i]){
+            min = c[i];
+            pos = i;
+        }
+        i++;
+    }
+    return pos;
+}
+
+int busquedaMonumento(Monumento* m,Monumento* resultado, Monumento& a, int n, int n2){
+    int c[n2];
+    for(int i=0; i<n2; i++){
+        c[i] = 0;
+    }
+    int numElementos = 0;
+    int minCoincidencias = 0;
+    int reemplazo = 0;
+    int coincidencias;
+    int i = 0;
+    while(i<n && minCoincidencias!=5){
+       coincidencias = compare(m[i],a);
+       if(coincidencias>minCoincidencias){
+           resultado[reemplazo]=m[i];
+           reemplazo = posDeReemplazo(c,n2);
+           minCoincidencias = c[reemplazo];
+           if(numElementos<5){
+               numElementos++;
+           }
+       }
+       i++;
+    }
+    //Ordenar de mayor a menor
+    Monumento aux1;
+    int aux2;
+    i = 0;
+    while(i<numElementos-1){
+        if(c[i]<c[i+1]){
+            aux1 = resultado[i];
+            aux2 = c[i];
+            resultado[i] = resultado[i+1];
+            c[i] = c[i+1];
+            resultado[i+1] = aux1;
+            c[i+1] = aux2;
+            if(i!=0){
+                i--;
+            }
+        }
+        else{
+            i++;
+        }
+    }
+}
+
